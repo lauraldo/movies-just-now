2ff552e7275ba1f8088133b2197ceee320f72c37
Nikita Iolas <lauraldoiolas@gmail.com>
2022-02-25 02:30:59 +0300

    TV shows detail screen

--- app/src/main/java/ru/androidschool/intensiv/ui/feed/FeedFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/feed/FeedFragment.kt
@@ -51,7 +51,6 @@ class FeedFragment : Fragment(R.layout.feed_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         searchBinding.searchToolbar.binding.searchEditText.afterTextChanged {
             Timber.d(it.toString())
             if (it.toString().length > MIN_LENGTH) {
